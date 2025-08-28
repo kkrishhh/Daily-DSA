7cd6e9b0be0f572c3cbf361644ef53422ee66152
kkrishhh <soham6218@gmail.com>
2025-08-28 22:37:43 +0530

    learnt about taking inputs in java

---
+++ src/javabasics/Main.java
@@ -0,0 +1,10 @@
+package javabasics;
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);//initializing scanner
+        String input = sc.nextLine();//sc.nextline has been activated and is stored in "input"
+        System.out.println(input);
+    }
+}

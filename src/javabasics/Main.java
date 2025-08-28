9645ecc6f181a26ce9f857aa05aed89c00ef0f07
kkrishhh <soham6218@gmail.com>
2025-08-28 22:51:45 +0530

    more input functions

--- src/javabasics/Main.java
+++ src/javabasics/Main.java
@@ -3,8 +3,21 @@ package javabasics;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);//initializing scanner
-        String input = sc.nextLine();//sc.nextline has been activated and is stored in "input"
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
         System.out.println(input);
     }
 }
+
+/*
+types of input function in java
+next()- takes single word as an input
+nextline()- takes whole line as input
+nextInt() - takes int as input
+nextByte ---||----
+nextFloat
+nextDouble
+nextBoolean
+nextShort
+nextLong
+ */

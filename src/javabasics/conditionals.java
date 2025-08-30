83ab6ac945dc5b20ba9580196aba8717d6729fb6
kkrishhh <soham6218@gmail.com>
2025-08-30 23:13:03 +0530

    conditionals

--- src/javabasics/conditionals.java
+++ src/javabasics/conditionals.java
@@ -1,5 +1,7 @@
 package javabasics;
 
+import java.util.Scanner;
+
 public class conditionals {
     public static void main(String[] args) {
         int num1 = 14;
@@ -9,5 +11,14 @@ public class conditionals {
         }else{
             System.out.println("num 2 is largest");
         }
+        //odd even
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        if(number % 2 == 0){
+            System.out.println("even number");
+        }else{
+            System.out.println("odd number");
+        }
+
     }
 }

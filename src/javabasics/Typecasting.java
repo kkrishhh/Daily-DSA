f787f68baedf31be45f2c4424f17ab25c9aabeea
kkrishhh <soham6218@gmail.com>
2025-08-30 22:56:20 +0530

    if else

--- src/javabasics/Typecasting.java
+++ src/javabasics/Typecasting.java
@@ -1,7 +1,18 @@
 package javabasics;
 
+import java.util.Scanner;
+
 public class Typecasting {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        float num = input.nextFloat();
+        System.out.println(num);
+        var age = 13;
+        if(age <= 18){//if else
+            System.out.println("can vote");
+        }else{
+            System.out.println("you cant");
+        }
 
     }
 }

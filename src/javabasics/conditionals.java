caff672bca9d89e62e9742de4c4eb1bc066dceef
kkrishhh <soham6218@gmail.com>
2025-08-30 23:06:02 +0530

    conditionals

---
+++ src/javabasics/conditionals.java
@@ -0,0 +1,13 @@
+package javabasics;
+
+public class conditionals {
+    public static void main(String[] args) {
+        int num1 = 14;
+        int num2 = 23;
+        if(num1 >= num2){
+            System.out.println("num 1 is largest");
+        }else{
+            System.out.println("num 2 is largest");
+        }
+    }
+}
